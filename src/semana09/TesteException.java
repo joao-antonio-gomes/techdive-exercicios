df6e8fba0eec697f713cd13c51934942e4462fcd
João Gomes <joaoantoniog96@gmail.com>
2022-01-23 19:44:12 -0300

    Correção tratamento de erro

---
+++ src/semana09/TesteException.java
@@ -0,0 +1,31 @@
+package semana09;
+
+public class TesteException {
+    public static void main(String[] args) {
+        System.out.println("inicio do main");
+        metodo1();
+        System.out.println("fim do main");
+    }
+
+    static void metodo1() {
+        System.out.println("inicio metodo 1");
+        try {
+            metodo2();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("erro: " + e.getMessage());
+        }
+        System.out.println("fim metodo 1");
+    }
+
+    static void metodo2() {
+        System.out.println("inicio metodo 2");
+        int[] array = new int[10];
+        for (int i = 0; i <= 15; i++) {
+            array[i] = i;
+            System.out.println(i);
+        }
+        System.out.println("fim metodo 2");
+    }
+
+
+}
