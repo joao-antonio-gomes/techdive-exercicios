413013c459cbf9a1d0aa85d0fc10bee42153ec2e
João Gomes <joaoantoniog96@gmail.com>
2022-01-28 10:20:10 -0300

    Exercicio Tres

---
+++ src/semana10/ExercicioTres.java
@@ -0,0 +1,21 @@
+package semana10;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import static java.time.temporal.TemporalAdjusters.firstInMonth;
+
+public class ExercicioTres {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        int todayYear = today.getYear();
+        for (int i = 1; i <= 12; i++) {
+            LocalDate monthLoop = LocalDate.of(todayYear, i, 1);
+            System.out.println("O primeiro sábado do mês " + monthLoop.getMonthValue() + " é no dia " +
+                    monthLoop.with(firstInMonth(DayOfWeek.SATURDAY)).format(DateTimeFormatter.ofPattern("dd/MM/YYYY")));
+        }
+    }
+}
