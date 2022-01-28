1ae2bcf0a2bff5e50082063f4b4e860efea3daa3
João Gomes <joaoantoniog96@gmail.com>
2022-01-28 11:08:18 -0300

    Exercicio 6

---
+++ src/semana10/ExercicioSeis.java
@@ -0,0 +1,18 @@
+package semana10;
+
+import java.io.*;
+
+public class ExercicioSeis {
+    public static void main(String[] args) throws IOException {
+        File file = new File("lista_de_filmes.txt");
+        String absolutePathFile = file.getAbsolutePath();
+        FileInputStream fis = new FileInputStream(absolutePathFile);
+        double bytes = fis.read();
+        double kilobytes = (bytes/1024);
+        double megabytes = (kilobytes/1024);
+
+        System.out.println("O arquivo tem: " + bytes + " bytes.");
+        System.out.println("O arquivo tem: " + kilobytes + " kilobytes.");
+        System.out.println("O arquivo tem: " + megabytes + " megabytes.");
+    }
+}
